a7a3cff97e57d8c7d06739d29a8837997e63823c
bozogae214 <n77333@gmail.com>
2025-05-10 00:33:22 +0900

    Update check_map_seat.py

--- check_map_seat.py
+++ check_map_seat.py
@@ -5,10 +5,11 @@ import time
 #######################################################
 ########### 아래 값 채워준 뒤 실행해주시면 됩니다. #############
 #######################################################
-prodId = ""
-pocCode = ""
-scheduleNo = ""
-cookie = ""
+prodId = "211309"
+pocCode = "SC0002"
+scheduleNo = "100001"
+cookie = "PCID=17246732088216913842539; PC_PCID=17246732088216913842539; _fwb=75ERfT1y01FZYsweBNzPiv.1726137882113; ab180ClientId=29b71157-b103-4a7b-b43f-b1f04433e468; TKT_POC_ID=WP15; airbridge_migration_metadata__melon=%7B%22version%22%3A%221.10.69%22%7D; _T_ANO=O0lafJ70et0GF6TWL9RvwHUfDV/+xkH1C5d/h2HUtb2x8WYMKcKBAqSkCgVl9LXDtV2J2GrGdqv+AopEwEGuwC4fkjOe6MMiB5KLKzb9AeVYwkIzghgnJ5aCNkTuqTxcKAThBsyY+H9NhZiiKIEKldXfJDscqdeOBlK9aXUuceeeqZ4C2B0E+r9RFjM3EgJK0oY7J82UBoBjFn4HTrZiaKGhqI0j88VY7kTQtsabbefzbvxR/OPEYxbfIxNLfWz0Z0yk5jaAgIGetqjr+Q1MGIyzTsj3t94U2nISHLQItWsh40y9aRwo+F+3r1yGTnqVDRmnouFUd591ZWmk3DClwg==; airbridge_session=%7B%22id%22%3A%22aca2abdf-0b18-4cd3-8ea8-b593280a5165%22%2C%22timeout%22%3A1800000%2C%22start%22%3A1746801771658%2C%22end%22%3A1746801782429%7D; MAC=s7IM+yDj04dPWttOum9/HjL5agSurNl9YCj3qzyL9V24WOxlLn8p2yNyd5Y1sOz3; MLCP=NTk3NTc2MjklM0JhZXJhbjEwMDYlM0IlM0IwJTNCZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKcGMzTWlPaUp0WlcxaVpYSXViV1ZzYjI0dVkyOXRJaXdpYzNWaUlqb2liV1ZzYjI0dGFuZDBJaXdpYVdGMElqb3hOelEyT0RBeE9ESXdMQ0p0WlcxaVpYSkxaWGtpT2lJMU9UYzFOell5T1NJc0ltRjFkRzlNYjJkcGJsbE9Jam9pVGlKOS5GQlpGa3JVZzJrV3M0eXFHZHhpYk1rSDc3YktlTXl5WWgyY19EOWtjV2xzJTNCJTNCMjAyNTA1MDkyMzQzNDAlM0JhZXJhbl8lM0IwJTNCYWVyYW4xMDA2JTNCNCUzQg==; MUS=694888852; keyCookie=59757629; store_melon_cupn_check=59757629; NetFunnel_ID=WP15; wcs_bt=s_585b06516861:1746804051; JSESSIONID=1F73FDC34581849A2A366ACEDD1C2A1A
+"
 slack_webhook_url = ""
 #######################################################
 #######################################################
